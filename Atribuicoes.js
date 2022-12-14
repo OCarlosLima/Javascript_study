5b27fdccde4630764e7f11a579f14b41588e606f
OCarlosLima <edulimandrade@hotmail.com>
2022-12-13 21:39:38 -0300

    Atribuições

--- Atribuicoes.js
+++ Atribuicoes.js
@@ -1,13 +1,30 @@
-// =
-// += ou a += b
-// -= ou a -+ b
-// *= ou a *= b
-// /= ou a /= b
-// %= ou a %= b
+var A = 10;
+var B = 20;
 
-var a = 10;
-var b = 10;
+// Atribui valor A para valor B
+console.log(A = B)
 
-a%=6;
+var A = A - 10 // RESET
 
-console.log(a);
+// Adiciona valor B para A
+console.log(A += B)
+
+var A = A - 20 // RESET
+
+// Diminui valor B de A
+console.log(A -= B)
+
+var A = A + 20
+
+// Multiplica o valor A por B vezes
+console.log(A *= B)
+
+var A = A - 190 // RESET
+
+// Divide o valor A por B
+console.log(A /= B)
+
+var A = A = 10 // RESET
+
+// Resto da divisão de A por B
+console.log(A %= B)
