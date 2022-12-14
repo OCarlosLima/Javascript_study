76961722fbb3dd2a45688a6e5b0c35f2adfeb776
OCarlosLima <edulimandrade@hotmail.com>
2022-12-13 21:27:43 -0300

    valores de comparaçao

--- Operadores_de_comparacao.js
+++ Operadores_de_comparacao.js
@@ -1,8 +1,27 @@
-// == igual a
-// === valor e tipo igual
-// != diferente de
-// !== valor e tipo diferente de
-// > maior que
-// < menor que
-// >= maior ou igual
-// <= menor ou igual
+var a = 10;
+var b = "10";
+var c = 20;
+
+// Igual a
+console.log(a == b)
+
+// Valor e tipo igal
+console.log(a === b)
+
+// Diferente de
+console.log(a != b)
+
+// Valor e tipo diferente
+console.log(a !== b)
+
+// Maior que
+console.log(a > c)
+
+// Menor que
+console.log(a < c)
+
+// Maior ou igual
+console.log(a >= b)
+
+// Menor ou igual
+console.log(a <= b)
