58a26a9a5074fbfd227fc305588cf697c11995ce
OCarlosLima <edulimandrade@hotmail.com>
2022-12-15 18:08:42 -0300

    nop

--- Operadores_logicos.js
+++ Operadores_logicos.js
@@ -2,18 +2,22 @@ var a = true;
 var b = false;
 
 // Ambos valores precisam ser verdadeiros
-console.log(a && b)
+console.log(a && b);
 
 // Um dos valores precisa ser verdaderos
-console.log(a || b)
+console.log(a || b);
+
+// Negativo
+console.log(!true)
+console.log(!false)
 
 console.log("------------------") // SEPARAÇÃO
 
-// Negativo
-var idade = 35
+// Analogia
+var idade = 25;
 
-var maior20 = idade >= 20
-var menor20 = idade < 20
+var maior20 = idade >= 20;
+var menor20 = !maior20;
 
-console.log("Idade maior que vinte?", maior20)
-console.log("Idade menor que vinte?", menor20)
+console.log("Idade maior que vinte?", maior20);
+console.log("Idade menor que vinte?", menor20);
