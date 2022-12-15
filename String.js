5e7010cf27d2a34c3346a9ddf9160b19edbbb3b2
OCarlosLima <edulimandrade@hotmail.com>
2022-12-15 18:31:33 -0300

    strings

---
+++ String.js
@@ -0,0 +1,17 @@
+// Pode se usar tanto aspas simples e duplas para string
+console.log('String com aspas simples');
+console.log("String com aspas duplas");
+
+// Para imprimir aspas dentro da string, deve haver uma diferente fora
+console.log('Texto "entre" aspas');
+
+// Pode concatenar textos com +=
+var str = "Texto " + "estando "
+str += "concatenado"
+console.log(str);
+
+// Pode concatenar com numeros tambem, transformando-os em strings
+console.log("Texto com o numero " + 8)
+
+// Numeros que são strings podem ser multiplicados, somados, subtraidos, etc
+console.log("8" * 2)
