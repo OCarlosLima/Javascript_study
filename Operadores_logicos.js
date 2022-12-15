540a248b1cccb7d6d1fc4d7ab855b63937c6cc5c
OCarlosLima <edulimandrade@hotmail.com>
2022-12-15 18:04:56 -0300

    Operadores logicos

--- Operadores_logicos.js
+++ Operadores_logicos.js
@@ -6,3 +6,14 @@ console.log(a && b)
 
 // Um dos valores precisa ser verdaderos
 console.log(a || b)
+
+console.log("------------------") // SEPARAÇÃO
+
+// Negativo
+var idade = 35
+
+var maior20 = idade >= 20
+var menor20 = idade < 20
+
+console.log("Idade maior que vinte?", maior20)
+console.log("Idade menor que vinte?", menor20)
