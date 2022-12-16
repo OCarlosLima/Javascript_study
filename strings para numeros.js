02964b5f25d75d4fc710568e801897dd69fa5c05
OCarlosLima <edulimandrade@hotmail.com>
2022-12-16 15:49:48 -0300

    str to number

---
+++ strings para numeros.js
@@ -0,0 +1,14 @@
+// Somar dois numeros que são uma string irá concatena-los
+console.log("3" + "6")
+
+// Numeros são impressos em azul, e strings em cinza (no console)
+console.log(10)
+console.log("10")
+
+// Para transformar string em numero inteiro use parseInt
+var a = "3.2";
+var b = "6.3";
+console.log(parseInt(a) + parseInt(b))
+
+// Em numeros reais, parseFloat
+console.log(parseFloat(a) + parseFloat(b))
