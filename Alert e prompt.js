f7860ae303481abeae310c19a89fbed26ce827b1
OCarlosLima <edulimandrade@hotmail.com>
2022-12-16 16:02:03 -0300

    prompt

---
+++ Alert e prompt.js
@@ -0,0 +1,11 @@
+// Alert faz um "alerta" no topo da pagina
+alert("Alerta")
+
+// Prompt abre uma caixa onde o usuario digita algo, podendo ser usada dentro de uma variavel para guardar um dado que pode ser posteriormente usado
+var info = prompt("Digite algo")
+alert("Você escreveu: " + info)
+
+// Pode ser usado com numeros e operações aritimeticas
+var numero = parseInt(prompt("Digite um número"))
+var dobro = numero * 2
+alert("O dobro do número " + numero + " é " + dobro)
