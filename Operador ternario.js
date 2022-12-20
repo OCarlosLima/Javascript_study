32445152f977d82c4ce48427d28ed45965810e99
OCarlosLima <edulimandrade@hotmail.com>
2022-12-20 18:22:02 -0300

    op ternarios

---
+++ Operador ternario.js
@@ -0,0 +1,11 @@
+// Operador de condição mais resumido do que IF e ELSE
+// Ultiliza-se ? para verificar se é verdade, e : para se é mentira (if else)
+var idade = 21;
+idade >= 18 ? console.log("Pode comprar") : console.log("Não pode comprar")
+
+console.log("---------------------"); // DIVISÃO
+
+// Pode usar para guardar um valor dentro de uma variável
+var idade = 15;
+var permitido = idade >= 18 ? true : false;
+console.log(permitido) // RESULTADO DA CONDIÇÃO
