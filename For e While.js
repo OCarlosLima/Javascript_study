a59ae345056844c3bad8dcf0eaedfa40d61300d4
OCarlosLima <edulimandrade@hotmail.com>
2022-12-22 15:10:54 -0300

    for & whilte part 1

---
+++ For e While.js
@@ -0,0 +1,7 @@
+// Comando For repete alguma coisa um certo número de vezes
+var numero = 5; // Quantidade de vezes que o For irá repetir
+var vez = 1;
+for (qntvez = 0; qntvez < numero; qntvez++) {
+    console.log("Executando pela vez número: " + vez++);
+}
+console.log("Acabou")
