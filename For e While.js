777d2212bfbdf08695bd6581bce87905805c8d4c
OCarlosLima <edulimandrade@hotmail.com>
2022-12-22 15:23:11 -0300

    for & white part 1

--- For e While.js
+++ For e While.js
@@ -1,7 +1,16 @@
 // Comando For repete alguma coisa um certo número de vezes
 var numero = 5; // Quantidade de vezes que o For irá repetir
-var vez = 1;
-for (qntvez = 0; qntvez < numero; qntvez++) {
-    console.log("Executando pela vez número: " + vez++);
+for (qntvez = 1; qntvez <= numero; qntvez++) {
+    console.log("Executando pela vez número: " + qntvez);
+}
+console.log("Acabou");
+
+console.log("-----------------------") // DIVISÃO
+
+// While verifica uma condição que, se for verdadeira, ira executar um bloco de comandos enquanto for verdadeiro
+var numero = 1;
+while(numero <= 5) {
+    console.log("Executando número: " + numero)
+    numero++;
 }
 console.log("Acabou")
